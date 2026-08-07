9e06df6ed9a1638fc57fccedbfbc11d359d55a91
shubh <shubhamfc1609@gmail.com>
2026-08-07 15:42:15 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+from xgboost import data
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -7,6 +9,8 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -46,4 +50,8 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    
